0726303f65a08c3eee712b32ffbced736cd658af
Harrison Ayan <harrisonayan@gmail.com>
2021-07-19 15:59:39 -0400

    added navbar component

---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,22 @@
+<template>
+  <nav class="navbar">
+    <ul>
+      <navbar-item name="Home" path="/"></navbar-item>
+      <navbar-item name="View" path="/view"></navbar-item>
+      <navbar-item name="Vote" path="/vote"></navbar-item>
+      <navbar-item name="Bid" path="/bid"></navbar-item> 
+    </ul>
+  </nav>
+</template>
+
+<script>
+import NavbarItem from '@/components/NavbarItem.vue'
+export default {
+  name: 'Navbar',
+  components: {
+    navbarItem: NavbarItem
+  }
+}
+</script>
+
+
---
+++ frontend/src/components/NavbarItem.vue
@@ -0,0 +1,19 @@
+<template>
+  <li class="navbar-item"><router-link :to=path>{{name}}</router-link></li>
+</template>
+
+<script>
+export default {
+  name: 'NavbarItem',
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    path: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
